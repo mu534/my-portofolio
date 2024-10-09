bea5868a55cf2d4a90725eb5e213cd9dd9bb218e
mu534 <144515613+mu534@users.noreply.github.com>
2024-10-09 15:04:33 +0000

    Fixed staticPublishPath directory

--- Assets/srcipt.js
+++ Assets/srcipt.js
@@ -1,7 +1,7 @@
-// srcipt.js
 function toggleMenu() {
-    const menuLinks = document.querySelector('.menu-links');
-   const icon = document.querySelector(".hamburger-icon");
-   menuLinks.classList.toggle('active'); // Toggles the active class
-   icon.classList.toggle('active'); // Toggles the active class
-}
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
